0903cfa832e235ff6b40637831cb627c7f12e833
ahmohamed1 <abdll1@hotmail.com>
2023-05-27 21:43:31 +0300

    restore old urdf

--- mycobot_280/mycobot_280/launch/test.launch.py
+++ mycobot_280/mycobot_280/launch/test.launch.py
@@ -4,10 +4,7 @@ from ament_index_python import get_package_share_path
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import Command, LaunchConfiguration
+
 
 
 def generate_launch_description():
